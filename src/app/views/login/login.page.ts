07a01a43371bb9ea82aaf65985a6a4bd19328d87
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-13 22:33:17 -0300

    add login page

--- src/app/views/login/login.page.ts
+++ src/app/views/login/login.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,58 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-
-  constructor() { }
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
+  loginForm: FormGroup;
+  isSubmitted = false;
+  isLoading: boolean;
+  route: Router;
+  constructor(public formBuilder: FormBuilder,
+    public loginService: LoginService,
+    private router: Router,) { }
 
   ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      login: ['', [Validators.required, Validators.minLength(2)]],
+      pass: ['', [Validators.required, Validators.minLength(2)]],
+    })
+  }
+
+  hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
+
+  get errorControl() {
+    return this.loginForm.controls;
+  }
+
+  EnterSubmit($event) {
+    if ($event.keyCode === 13) {
+      this.submitForm();
+    }
+  }
+
+  async submitForm() {
+    this.isSubmitted = true;
+    var login = this.loginForm.value.login;
+    var pass = this.loginForm.value.pass;
+
+    this.isLoading = true;
+    this.loginService
+      .login({ login, pass })
+      .subscribe(
+        (data) => {
+          this.isLoading = false;
+          this.router.navigate(['/home']);
+        },
+        (error) => {
+          console.error(error);
+          this.isLoading = false;
+          this.router.navigate(['/login']);
+        }
+      );
+
   }
 
 }
