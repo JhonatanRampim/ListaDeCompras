cf63fa74d609e2aeac3297e0e815071ed2a0cc1d
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-13 21:58:48 -0300

    started login page and login service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -18,6 +18,10 @@ const routes: Routes = [
   {
     path: 'lista',
     loadChildren: () => import('./views/lista/lista.module').then( m => m.ListaPageModule)
+  },
+  {
+    path: 'login',
+    loadChildren: () => import('./views/login/login.module').then( m => m.LoginPageModule)
   }
 ];
 
