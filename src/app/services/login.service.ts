174274e725418320e589f8721c87d36ee39bb4df
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-13 22:33:08 -0300

    add login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import jwt_decode from "jwt-decode";
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
+  private userSubject: BehaviorSubject<any>;
+  public user: Observable<any>;
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+    this.userSubject = new BehaviorSubject<any>((localStorage.getItem('token')) ? jwt_decode(localStorage.getItem('token')) : null);
+    this.user = this.userSubject.asObservable();
+   }
+  public get userValue() {
+    return this.userSubject.value;
+  }
+  login(data?) {
+    return this.httpClient.post(environment.apiUrl + 'auth/login', {email:data.login, password:data.pass})
+      .pipe(map(user => {
+        // localStorage.setItem('token', user.token);
+        this.userSubject.next(localStorage.getItem('token'));
+        return user;
+      }));
+  }
 }
