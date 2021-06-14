337f212b8270ec6c38303b5df7c63dc0d9fd7155
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-13 21:31:04 -0300

    add getmylistitems in service

--- src/app/services/listas.service.ts
+++ src/app/services/listas.service.ts
@@ -11,7 +11,12 @@ export class ListasService {
   constructor(private httpClient: HttpClient) { }
 
   getMyLists() {
-    return this.httpClient.post(this.apiUrl + 'lista/geyMyList', { id: 1 })
-      .pipe(map(lista => { lista }));
+    return this.httpClient.post<any>(this.apiUrl + 'lista/getMyList', { id: "1" })
+      .pipe(map(lista => lista));
+  }
+
+  getMyListsItems(id?) {
+    return this.httpClient.post<any>(this.apiUrl + 'lista/getMyListWithItems', { id: null, id_lista: id })
+      .pipe(map(lista => lista));
   }
 }
