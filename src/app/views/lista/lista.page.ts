ea7c040fcd3d82df4f5167740f716f84c3e14b68
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-19 13:18:21 -0300

    modal lista

--- src/app/views/lista/lista.page.ts
+++ src/app/views/lista/lista.page.ts
@@ -10,20 +10,24 @@ import { ListasService } from 'src/app/services/listas.service';
 export class ListaPage implements OnInit {
   public ListsItems: Array<any>;
   public Items: any;
+  isLoading: boolean = false;
   @Input() id_lista: string;
   @Input() nome_lista: string;
   constructor(public modalCtrl: ModalController, private listaService: ListasService) { }
 
   ngOnInit() {
+    this.isLoading = true;
     this.getMyListsItems();
   }
   getMyListsItems() {
+
     this.listaService.getMyListsItems(this.id_lista).subscribe(response => {
       this.ListsItems = response.data
       this.ListsItems.forEach(arrayItems => {
         this.Items = arrayItems.items;
       });
     });
+    this.isLoading = false;
   }
 
   closeModal() {
