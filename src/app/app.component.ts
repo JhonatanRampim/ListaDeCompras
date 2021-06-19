cc374712b9b70624e9e5d21a9c247e308641f418
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-19 16:47:16 -0300

    melhorias de validação de erro no menu logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserModel } from './models/user.model';
+import { LoginService } from './services/login.service';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   public appPages = [
     { title: 'Tela Inicial', url: '/home', icon: 'home' },
     { title: 'Criar Lista', url: '/criarlista', icon: 'add-circle' },
   ];
-  constructor() {}
+  user: UserModel;
+  constructor(private loginService: LoginService, private router: Router) { }
+  ngOnInit() {
+    this.loginService.user.subscribe(user => {
+    if(user){
+      this.user = user 
+    } 
+    });
+  }
+  logout() {
+    this.loginService.logout().subscribe(data => {
+      if (data) {
+        this.router.navigate(['/login']);
+      }
+    });
 
+  }
 }
