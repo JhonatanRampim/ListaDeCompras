49259149d87a64f59437306929189e6c9da4c111
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-27 13:02:01 -0300

    layout improvements add new item on edit

--- src/app/views/modals/lista/lista.page.ts
+++ src/app/views/modals/lista/lista.page.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ModalController } from '@ionic/angular';
+import { Item } from 'src/app/models/lista.model';
 import { ListasService } from 'src/app/services/listas.service';
 
 @Component({
@@ -11,6 +12,7 @@ export class ListaPage implements OnInit {
   public ListsItems: Array<any>;
   public Items: any;
   isLoading: boolean = false;
+  newItems: Item = {};
   @Input() id_lista: string;
   @Input() nome_lista: string;
   constructor(public modalCtrl: ModalController, private listaService: ListasService) { }
@@ -32,5 +34,12 @@ export class ListaPage implements OnInit {
 
   closeModal() {
     this.modalCtrl.dismiss();
+  }
+  addNewItem() {
+    this.Items.push(this.newItems);
+    this.newItems = {};
+  }
+  salvarEdit() {
+    console.log(this.ListsItems);
   }
 }
