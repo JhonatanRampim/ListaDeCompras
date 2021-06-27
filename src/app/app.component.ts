8742ef0c388000e57e75872eb2f7adc63f2fea52
Jhonatan Eduardo Rampim <jhonatan.rampim@gmail.com>
2021-06-27 20:10:48 -0300

    improvements on icons changed manifest

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { SwUpdate } from '@angular/service-worker';
 import { MenuController } from '@ionic/angular';
 import { UserModel } from './models/user.model';
 import { LoginService } from './services/login.service';
@@ -14,14 +15,18 @@ export class AppComponent implements OnInit {
     { title: 'Criar Lista', url: '/criarlista', icon: 'add-circle' },
   ];
   user: any;
-  constructor(private loginService: LoginService, private router: Router, private menuController:MenuController) { }
+  constructor(private loginService: LoginService, private router: Router, private menuController: MenuController, updates: SwUpdate) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
   ngOnInit() {
     this.loginService.user.subscribe(user => {
-    if(user){
-      this.menuController.swipeGesture(true);
-      return this.user = user 
-    } 
-    this.menuController.swipeGesture(false);
+      if (user) {
+        this.menuController.swipeGesture(true);
+        return this.user = user
+      }
+      this.menuController.swipeGesture(false);
     });
   }
   logout() {
